6c0d01beb930ab63f60febbaa6ddb7b60458a751
Janderion <jp.unknown47@gmail.com>
2025-09-08 10:42:18 -0400

    Update Code1.py

--- Code1.py
+++ Code1.py
@@ -5,19 +5,38 @@ Created on Mon Sep  8 09:43:06 2025
 @author: jadenpaula
 """
 
+from decimal import Decimal, getcontext
 #from uncertainties import ufloat
 
+getcontext().prec = 3
+
 Unit_measurement = 100000 # Per this many people
-
 InitialPopulation=int(input('Beginning Population?: '))
+ConvertStat=Decimal(Unit_measurement/InitialPopulation)
 
-Prevalence = int(input("How many affected?: "))*Unit_measurement/InitialPopulation
+Prevalence = Decimal(input("How many affected?: "))*ConvertStat
 print("Prevalence:", Prevalence, "per", Unit_measurement, "people were affected.")
 
-Incidence = int(input("How many new people affected?: "))*Unit_measurement/InitialPopulation
+Incidence = Decimal(input("How many new people affected?: "))*ConvertStat
 print("Incidence: ",Incidence, "per", Unit_measurement, "was the incidence rate for the time period.")
 
-MortalityRate=int(input("How many people died?: "))*Unit_measurement/InitialPopulation
+MortalityRate=Decimal(input("How many people died?: "))*ConvertStat
 print("Mortality Rate:", MortalityRate, "per", Unit_measurement, "people died for the time period.")
 
-# TODO: Need to add calculation of the years lost
+
+CalcYearsLost = input("Calculate years of potential life lost?: ")
+if CalcYearsLost=="y":
+    CalcYearsLost=True
+else:
+    CalcYearsLost=False
+
+LifeExpectation = None
+
+while CalcYearsLost:
+    if LifeExpectation==None:
+        LifeExpectation= float(input("What is the life expectancy age?: "))
+    else:
+        question1=int(input("What was the age of a person who died? (answer 0 if no ages are known): "))
+        
+        difference=LifeExpectation-question1
+        print("This person lost", difference, "years of life")
