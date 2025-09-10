bfe430f9d3553e06d780ac1df99618bd6662459a
Janderion <jp.unknown47@gmail.com>
2025-09-10 09:50:19 -0400

    Update Code1.py

--- Code1.py
+++ Code1.py
@@ -15,13 +15,13 @@ InitialPopulation=int(input('Beginning Population?: '))
 ConvertStat=Decimal(Unit_measurement/InitialPopulation)
 
 Prevalence = Decimal(input("How many affected?: "))*ConvertStat
-print("Prevalence:", Prevalence, "per", Unit_measurement, "people were affected.")
+print("Prevalence:", Prevalence, "per", Unit_measurement, "people affected.")
 
 Incidence = Decimal(input("How many new people affected?: "))*ConvertStat
-print("Incidence: ",Incidence, "per", Unit_measurement, "was the incidence rate for the time period.")
+print("Incidence: ",Incidence, "new cases per", Unit_measurement, "people.")
 
 MortalityRate=Decimal(input("How many people died?: "))*ConvertStat
-print("Mortality Rate:", MortalityRate, "per", Unit_measurement, "people died for the time period.")
+print("Mortality Rate:", MortalityRate, "deaths per", Unit_measurement, "people.")
 
 
 CalcYearsLost = input("Calculate years of potential life lost?: ")
@@ -36,7 +36,11 @@ while CalcYearsLost:
     if LifeExpectation==None:
         LifeExpectation= float(input("What is the life expectancy age?: "))
     else:
-        question1=int(input("What was the age of a person who died? (answer 0 if no ages are known): "))
+        try:
+            question1=int(input("What was the age of a person who died? (Provide no answer if no ages are known): "))
+            difference=LifeExpectation-question1
+            print("This person lost", difference, "years of life")
+        except ValueError:
+            CalcYearsLost = False
         
-        difference=LifeExpectation-question1
-        print("This person lost", difference, "years of life")
+        
